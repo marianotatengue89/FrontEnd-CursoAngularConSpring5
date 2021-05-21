8e6807b64f5cb1ad1242f294491b0215485d0368
mariano_tate <mariano.tatengue@gmail.com>
2021-05-21 17:07:25 -0300

    Primer Commit con el agregado del component Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clientes-app';
+
+  curso: string = 'Curso Spring 5 con Angular 12';
+  profesor: string = 'Mariano J. Villalba';
 }
