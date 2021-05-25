256654875bcd333b90b09aeb7ecdbf42ea60a91c
mariano_tate <mariano.tatengue@gmail.com>
2021-05-25 02:43:05 -0300

    Se crea el componente clientes y se los muestra de manera statica en una tabla de bootstrap.

---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,20 @@
+export class Cliente {
+    id: number;
+    nombre: string;
+    apellido: string;
+    createAt: string;
+    email: string;
+
+    constructor() { 
+        this.id = -1;
+        this.nombre = '';
+        this.apellido = '';
+        this.email = '';
+        this.createAt = '';
+    }
+
+
+}
+
+
+
