d3b534b909ee8f7260ce367ecdef9f1ee40ba422
mariano_tate <mariano.tatengue@gmail.com>
2021-05-25 12:19:31 -0300

    Video 571 572 573 y 574 - Se separan los datos, se crea el clienteService y se hace uso de la inyeccion de dependencias, y se hace una introduccion a la programacion reactiva y uso de observables

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente'
+import { ClienteService } from './cliente.service';
 
 @Component({
   selector: 'app-clientes',
@@ -9,22 +10,14 @@ import { Cliente } from './cliente'
 
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {"id":1,"nombre":"Bronny","apellido":"De Meyer","email":"bdemeyer0@wufoo.com","createAt":"28/12/2020"},
-    {"id":2,"nombre":"Kayla","apellido":"Grainger","email":"kgrainger1@pbs.org","createAt":"20/03/2021"},
-    {"id":3,"nombre":"Annetta","apellido":"Frowde","email":"afrowde2@ning.com","createAt":"02/11/2020"},
-    {"id":4,"nombre":"Glyn","apellido":"Tarbard","email":"gtarbard3@rakuten.co.jp","createAt":"03/01/2021"},
-    {"id":5,"nombre":"Cate","apellido":"Franzettoini","email":"cfranzettoini4@youku.com","createAt":"07/06/2020"},
-    {"id":6,"nombre":"Sam","apellido":"Fishleigh","email":"sfishleigh5@uiuc.edu","createAt":"10/04/2021"},
-    {"id":7,"nombre":"Alan","apellido":"Grellier","email":"agrellier6@aboutads.info","createAt":"07/02/2021"},
-    {"id":8,"nombre":"Cordie","apellido":"Keggin","email":"ckeggin7@usda.gov","createAt":"13/02/2021"},
-    {"id":9,"nombre":"Isa","apellido":"Langmuir","email":"ilangmuir8@google.com.hk","createAt":"08/10/2020"},
-    {"id":10,"nombre":"Nathalia","apellido":"Rentilll","email":"nrentilll9@merriam-webster.com","createAt":"11/09/2020"}
-  ];
+  clientes: Cliente[]; 
 
-  constructor() { }
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
+    this.clienteService.getClientes().subscribe(
+      clientes => this.clientes = clientes
+    ); 
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente'
+
+
+export const CLIENTES: Cliente[] = [
+    {"id":1,"nombre":"Bronny","apellido":"De Meyer","email":"bdemeyer0@wufoo.com","createAt":"28/12/2020"},
+    {"id":2,"nombre":"Kayla","apellido":"Grainger","email":"kgrainger1@pbs.org","createAt":"20/03/2021"},
+    {"id":3,"nombre":"Annetta","apellido":"Frowde","email":"afrowde2@ning.com","createAt":"02/11/2020"},
+    {"id":4,"nombre":"Glyn","apellido":"Tarbard","email":"gtarbard3@rakuten.co.jp","createAt":"03/01/2021"},
+    {"id":5,"nombre":"Cate","apellido":"Franzettoini","email":"cfranzettoini4@youku.com","createAt":"07/06/2020"},
+    {"id":6,"nombre":"Sam","apellido":"Fishleigh","email":"sfishleigh5@uiuc.edu","createAt":"10/04/2021"},
+    {"id":7,"nombre":"Alan","apellido":"Grellier","email":"agrellier6@aboutads.info","createAt":"07/02/2021"},
+    {"id":8,"nombre":"Cordie","apellido":"Keggin","email":"ckeggin7@usda.gov","createAt":"13/02/2021"},
+    {"id":9,"nombre":"Isa","apellido":"Langmuir","email":"ilangmuir8@google.com.hk","createAt":"08/10/2020"},
+    {"id":10,"nombre":"Nathalia","apellido":"Rentilll","email":"nrentilll9@merriam-webster.com","createAt":"11/09/2020"}
+  ];
