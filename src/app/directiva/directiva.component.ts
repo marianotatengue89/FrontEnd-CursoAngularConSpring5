03a708329129823d11e1fccd76f4d8ec5e3266aa
mariano_tate <mariano.tatengue@gmail.com>
2021-05-25 00:33:24 -0300

    Se crea el componente directiva para hacer uso de la directiva *ngFor

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+})
+export class DirectivaComponent {
+  
+  listaCursos: string[] = ['TypeScript', 'JavaScript', 'Java', 'c#', 'PHP'];
+
+  constructor() {
+
+
+   }
+
+}
